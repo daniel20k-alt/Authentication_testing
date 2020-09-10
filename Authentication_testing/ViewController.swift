77710f915e94a1ab94e2de270098674231c44594
Testing_author <testing_beta@mail.com>
2020-09-10 23:05:28 +0300

    Made the adjustForKeyboard function, which will handle the on-screen keyboard for the text editor.

--- Authentication_testing/ViewController.swift
+++ Authentication_testing/ViewController.swift
@@ -9,14 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var hiddenTextEditor: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let notificationCenter = NotificationCenter.default
+        //when the keyboard shows or hides we should be notified
+        
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
-
+    
     @IBAction func authenticateTapped(_ sender: Any) {
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keyboardScreenEnd = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEnd, from: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            hiddenTextEditor.contentInset = .zero
+        } else {
+            hiddenTextEditor.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+        
+        hiddenTextEditor.scrollIndicatorInsets = hiddenTextEditor.contentInset
+        
+        let selectedRange = hiddenTextEditor.selectedRange
+        hiddenTextEditor.scrollRangeToVisible(selectedRange)
     }
 }
 
